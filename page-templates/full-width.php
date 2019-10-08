ee3faa7665b2b1d4e4d15d4c5b0dc773cb06da96
Raúl Pérez <raulperez0481@gmail.com>
2019-10-08 11:01:44 +0200

    Mejora en breadcrumbs

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -9,7 +9,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <article class="article-full">
 
-          <h2><?php the_title(); ?></h2>
+          <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
 
         <?php the_content(); ?>
       </article>
