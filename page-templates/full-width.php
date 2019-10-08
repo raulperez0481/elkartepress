57c1f6b2b1026d832cd362e51e878d72a0d30d0b
Raúl Pérez <raulperez0481@gmail.com>
2019-10-08 10:53:08 +0200

    Añadida breadcrums

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -8,9 +8,9 @@
   <section class="content-area content-full-width">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <article class="article-full">
-        <header>
+
           <h2><?php the_title(); ?></h2>
-        </header>
+
         <?php the_content(); ?>
       </article>
 <?php endwhile; else : ?>
